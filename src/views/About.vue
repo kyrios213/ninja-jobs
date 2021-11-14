195bf74ad237c557f0715afee89fe89e8ffa51c3
Lance Pidor <kyrios213@gmail.com>
2021-11-14 16:57:27 +0800

    Commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+      exercitationem optio eos provident eligendi obcaecati dolores debitis,
+      harum necessitatibus nihil pariatur eveniet alias a nam fugiat cumque
+      accusamus saepe, omnis iste quasi maiores, placeat veniam laudantium nisi!
+      Voluptate maxime amet sunt perferendis in magni beatae asperiores eligendi
+      quas? Eligendi ipsum inventore magnam obcaecati consequuntur debitis earum
+      vel, neque nesciunt provident accusantium eos a dolore tempora
+      exercitationem error doloremque corporis nemo modi distinctio quaerat id
+      perspiciatis labore soluta. Mollitia doloremque illum assumenda, ducimus
+      provident illo cumque adipisci voluptates, accusantium ad aliquid,
+      repellendus maiores quia eveniet reiciendis sed commodi repudiandae
+      nostrum laudantium?
+    </p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias est,
+      quibusdam vero explicabo rem laborum, voluptatum itaque asperiores cumque
+      omnis nam repellat perferendis deserunt beatae nulla veniam iure id
+      molestiae quos. Repellat ratione repudiandae iure accusantium quaerat
+      consectetur dolor obcaecati necessitatibus, corporis deleniti aliquid
+      suscipit, nulla nostrum totam ab odio dolorum voluptatum nihil quia
+      voluptatibus. Nemo laborum natus consequuntur ut, doloribus corrupti
+      voluptatum excepturi, dolore molestias beatae exercitationem vero
+      consectetur molestiae inventore sint, repellat libero accusamus aut. Illo
+      odit, porro quos optio at aperiam nam impedit accusantium ea consequatur
+      architecto quam, deserunt repudiandae esse beatae sint ratione pariatur
+      voluptatem! Sint.
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
