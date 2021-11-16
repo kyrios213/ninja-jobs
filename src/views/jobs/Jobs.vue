cb8e7c2a90362acd24ab1b7390b05a43d17bbfe0
Lance Pidor <kyrios213@gmail.com>
2021-11-16 10:52:15 +0800

    Done

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,9 +1,15 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobsDetails', params: { id: job.id } }"
-      ><h2>{{ job.title }}</h2></router-link
-    >
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobsDetails', params: { id: job.id } }"
+        ><h2>{{ job.title }}</h2></router-link
+      >
+    </div>
+  </div>
+
+  <div v-else>
+    <p>Loading Jobs....</p>
   </div>
 </template>
 
@@ -11,12 +17,20 @@
 export default {
   data() {
     return {
-      jobs: [
-        { title: "Ninja UX Designer", id: 1, details: "lorem" },
-        { title: "Ninja web Developer", id: 2, details: "lorem" },
-        { title: "Ninja Vue Developer", id: 3, details: "lorem" },
-      ],
+      jobs: [],
     };
+  },
+  async mounted() {
+    try {
+      const res = await fetch("http://localhost:3000/jobs");
+      const data = await res.json();
+      this.jobs = data;
+    } catch (err) {
+      console.log(err.message);
+    }
+    // fetch("http://localhost:3000/jobs")
+    //   .then((res) => res.json())
+    //   .then((data) => (this.jobs = data));
   },
 };
 </script>
--- src/views/jobs/JobsDetails.vue
+++ src/views/jobs/JobsDetails.vue
@@ -1,11 +1,32 @@
 <template>
-  <h1>Jobs Details Page</h1>
-  <p>The job id is {{ id }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>The job id is {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+
+  <div v-else>
+    <p>Loading jobs details</p>
+  </div>
 </template>
 
 <script>
 export default {
   props: ["id"],
+  data() {
+    return {
+      job: null,
+    };
+  },
+  async mounted() {
+    try {
+      const res = await fetch(`http://localhost:3000/jobs/${this.id}`);
+      const data = await res.json();
+      this.job = data;
+    } catch (err) {
+      console.log(err.message);
+    }
+  },
   //   data() {
   //     return {
   //       id: this.$route.params.id,
